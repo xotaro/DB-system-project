d40d8f074b44f733399ccebb66e6c393d46cd12c
xotaro <dragonforallsss@gmail.com>
2020-05-18 20:47:16 +0200

    on

---
+++ src/ManGaH/DBAppTest.java
@@ -0,0 +1,30 @@
+package ManGaH;
+
+import java.awt.Polygon;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Hashtable;
+import java.util.Vector;
+import java.util.Iterator;
+import java.util.Random;
+import BpTree.Ref;
+
+public class DBAppTest {
+	private static final Random rand = new Random();
+
+	public static void main(String[] args) throws DBAppException, IOException, ParseException {
+
+		
+
+	}
+
+}
